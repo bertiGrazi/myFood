9c4031de040bcc0520721624d4cfa62bdd4d7ca7
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-01-31 20:12:30 -0300

    create app and folders

---
+++ myFood/myFood/Controllers/HomeViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  myFood
+//
+//  Created by Grazielli Berti on 31/01/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
+
