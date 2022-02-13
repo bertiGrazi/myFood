b789e0b3c71fb0209b43a8760a204171c6de3b11
Grazielli Lima Berti <limaberti.grazi@gmail.com>
2022-02-13 10:12:27 -0300

    create restaurant list api

---
+++ Coordinator/MyFood/MyFood/Service/Models/RestaurantListModel.swift
@@ -0,0 +1,17 @@
+//
+//  RestaurantListModel.swift
+//  MyFood
+//
+//  Created by Grazielli Berti on 13/02/22.
+//
+import Foundation
+
+class RestaurantListModel {
+    var nameRestaurant: String
+    var categoryRestaurant: String
+    
+    init(nameRestaurant: String, categoryRestaurant: String) {
+        self.nameRestaurant = nameRestaurant
+        self.categoryRestaurant = categoryRestaurant
+    }
+}
